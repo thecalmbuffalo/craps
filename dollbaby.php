48199c74a9fb9fd182e40777303427ddd61eb841
thecalmbuffalo <53958113+thecalmbuffalo@users.noreply.github.com>
2019-08-27 10:52:11 +0000

    Create dollbaby.php

---
+++ dollbaby.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+$conn = new mysqli($servername, $username, $password);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE {
+    echo "Database created successfulle";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
