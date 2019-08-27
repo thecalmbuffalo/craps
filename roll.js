a097d981d363686063a6464b69de762c4676c3cf
thecalmbuffalo <53958113+thecalmbuffalo@users.noreply.github.com>
2019-08-27 11:48:50 -0500

    Update roll.js

--- roll.js
+++ roll.js
@@ -1,5 +1,5 @@
-const canvas = document.getElementById('my-roll');
+const canvas = document.getElementById('rollHere');
 const ctx = canvas.getContext('2d');
 ctx.lineWidth = 5;
 ctx.strokeRect(75, 75, 75, 75);
-ctx.strokeRect(75, 75, 75, 75);
+ctx.strokeRect(75, 180, 75, 75);
