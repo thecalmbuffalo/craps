94146cea66d52e9af3b9bb0c27ab6ba0b2e5928e
thecalmbuffalo <53958113+thecalmbuffalo@users.noreply.github.com>
2019-08-27 13:20:31 -0500

    Update roll.js

--- roll.js
+++ roll.js
@@ -3,5 +3,5 @@ const ctx = canvas.getContext('2d');
 ctx.lineWidth = 5;
 ctx.strokeRect(75, 75, 75, 75);
 ctx.strokeRect(180, 75, 75, 75);
-var cube = ["1", "2", "3", "4", "5", "6"];
-CanvasRenderingContext2D.strokeText(cube, 12, 12[, maxWidth]);
+ctx.strokeText("1", 90, 85, 35, 35,);
+ctx.strokeText("6", 195, 85, 35, 35,);
